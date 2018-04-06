316d3e8ac4fc66c8a8677a2d6fe131f13858092b
nablazek <nablazek@cretecarrier.com>
2018-04-06 13:58:33 -0500

    added some comments and cadded the ability to inject custom changes to the regex and/or output. I also added some javadocs and instructions

--- README.md
+++ README.md
@@ -1,16 +1,51 @@
 # Welcome to ActiveType Markdown Parser!
 ## By Nathan Blazek
+### Installation and Setup
+1. Place the following in the &lt;head&gt; section of your webpage. Make sure the path is correct and relative to where you installed the js file.<br />
+            `<script src="../src/active-type.js"></script>`
+2. Add the following to the bottom of your html, just above the &lt;/body&gt; tag
+               
+```javascript
+    <script type="text/javascript">
+       $(document).ready(function() {
+           var options = {
+               textarea_selector:'#active-type',
+               preview_selector:'#active-type-preview',
+               markdown:{ //Enter custom regex in here, identical regex will overwrite defaults
+                   'italics':'&lt;em&gt;$2&lt;/em&gt;',
+               },
+               regex:[ //Custom regex using this structure
+                   {   name:'Box',
+                       regex:/{{Box:([0-9]*) title:(.*)}}/g,
+                       replace:'&lt;div class="card" id="box_$1"&gt;$2&lt;/div&gt;'
+                   }
+               ]
+    
+           };
+    
+           $("#active-type").activeType(options);
+       });
+    </script>
+```
+               
+
+
 Thank you for checking out my plugin. I created this plugin for a personal project of mine located at [my personal web site](http://www.nateblazek.com)...eventually!
 ### Goals
 1. Actively parse markdown and display it on the webpage as a user updates it.
 12. Process markdown by using basic regex
 33. Get better at using regex..**I suck at it**, I really do. I use it very periodically and I never get held up by it, but I do not feel confident enough to use it without the help of [Google](http://www.google.com) or online test sites like [Regex101](http://www.regex101.com)
-3. Allow Regex to be passed into the plugin. This, at it's core, was a requirement of mine. I want to store markdown in a table, parsing it server side to deliver onto a webpage. Rewriting regex didnt sound like fun and I hate code duplication. Allowing regex to be passed into this plugin solves my concerns.
+3. Allow Regex to be passed into the plugin. This, at it's core, was a requirement of mine. I want to store markdown in a table, parsing it server side to deliver onto a webpage. Rewriting regex didnt sound like fun and I hate code duplication. Allowing regex to be passed into this plugin solves my concerns. *Hopefully*.
 ------------
 ### Markdown Logic
-> The markdown sample can be taken from this README.md file. This is standard markdown code, commonly used in github projects.
-
-`If you want to use code, you'd put it in a block of text like this and it would not get rendered on output. <strong> SEE!</strong>`
+* \# Headers - They are created with \#, where the number of \# determines the size of the header
+* Links - They are created with the \[Link Title](Link URL) format.
+* \*\*Bold\*\* - Put any text in between two asterisk to bold the text.
+* \*Italics\* - Put any text between a single asterisk to italicize the text.
+* \`code\` - Put your text between these characters(What are they called? No idea..) to emphasize your code. Browsers/websites tend to put this in a monospace format by default
+* \> Blockqoute - Put your text after a greater than symbol to create blockquotes.
+* \* Unordered List - Put an asterisk before your text to make an unordered list. Having these in each line will group them together into one unordered list.
+* 5\. Ordered List - Put an asterisk before your text to make an ordered list. Having these in each line will group them together into one ordered list.
 
 
 
--- src/active-type.js
+++ src/active-type.js
@@ -23,9 +23,112 @@
         var saveFrequency = 15000; //15 seconds
         var lastChecked = new Date().getTime();
 
+        var defaultRegex = [
+            {name:'header', regex:/(#+)(.*)/g,                  replace: header},
+            {name:'bold',   regex:/(\*\*|__)(.*?)\1/g,          replace: '<strong>$2</strong>'},
+            {name:'link',   regex:/\[([^\[]+)\]\(([^\)]+)\)/g,  replace: '<a href=\'$2\'>$1</a>'},
+            {name:'rule',   regex:/\n-{5,}/g,                   replace: '\n<hr />'},
+            {name:'code',   regex:/`(.*?)`/g,                   replace: '<code>$1</code>'},
+            {name:'italics',regex:/(\*|_)(.*?)\1/g,             replace: '<em>$2</em>'},
+            {name:'bquote', regex:/\n(&gt;|\>)(.*)/g,           replace: '\n<blockquote>$2</blockquote>'},
+            {name:'ul',     regex:/((?:\*.*\n)+)/gm,            replace: list_ul},
+            {name:'ol',     regex:/((?:[0-9]+\..*\n)+)/gm,      replace: list_ol},
+            {name:'para',   regex:/\n([^\n]+)\n/gm,             replace: paragraph},
+            {name:'cleanup',regex:/\\/gm,                       replace: ''}
+        ];
+
+        /* REGEX FUNCTIONS */
+
+        /**
+         * Processes header markdown, while counting the # signs to determine header size
+         * @param match - not used
+         * @param contents
+         * @param offset
+         * @returns {string}
+         */
+        function header(match, contents, offset){
+            var num = contents.length;
+            return '<h' + num + '>'+offset.trim()+'</h' + num + '>';
+        }
+
+        /**
+         * For unordered lists
+         * Processes the list group as a whole, then breaks down each list item
+         * @param entire_group
+         * @returns {string}
+         */
+        function list_ul(entire_group){
+            // Returns the entire li group since the regex grabbed all the items in the grouping
+            // Now I can add <ul> around the results and <li> tags around each list item
+
+            var result = entire_group.trim();
+            result = result.replace(/\*(.*)/g,'<li>$1</li>');
+            return '<ul>'+result+'</ul>';
+        }
+
+        /**
+         * For ordered lists
+         * Processes the list group as a whole, then breaks down each list item
+         * @param entire_group
+         * @returns {string}
+         */
+        function list_ol(entire_group){
+            // Returns the entire li group since the regex grabbed all the items in the grouping
+            // Now I can add <ol> around the results and <li> tags around each list item
+
+            var result = entire_group.trim();
+            result = result.replace(/^[0-9]*\.(.*)/gm,'<li>$1</li>');
+            return '<ol>'+result+'</ol>';
+        }
+
+        /**
+         * Adds <p> tags around blocks of text, as long as it does not overlap a closing html tag
+         * @param match - not used
+         * @param content
+         * @returns {string}
+         */
+        function paragraph(match, content) {
+            var result = content.trim();
+            // Prevent insertion of <p> tag inside an existing tag that should not contain p tags
+            if (/^<\/?(h|p|bl|ul|ol|li)/i.test(result)) {
+                return '\n' + content + '\n';
+            }
+            return '\n<p>' + result + '</p>\n';
+        }
+
+
+        /**
+         * Renders text by using the regex, returning the resulting string
+         * @param originalText
+         * @returns {string}
+         */
+        function render(originalText){
+            var resultText = originalText;
+
+            //Process each regular expression
+            $.each(defaultRegex,function(index,item){
+                console.log(item);
+                var replacement = (options.markdown && options.markdown[item.name])?options.markdown[item.name]: item.replace;
+                resultText = resultText.replace(item.regex,replacement);
+            });
+
+            //Process custom regex
+            $.each(options.regex,function(index,item){
+                resultText = resultText.replace(item.regex,item.replace);
+            });
+
+            return resultText;
+        }
+
+        /* PROCESSING FUNCTIONS */
         //I want to save textarea changes every 15 seconds, but only if the textarea updated
-       $(document).on('change keyup',options.selector,function() {
-        updatePreview();
+        $(document).on('change keyup',options.selector,function() {
+
+            var result = render($(options.textarea_selector).val());
+
+            //place results into the preview container
+            $(options.preview_selector).html(result);
+
             var thisTime = new Date().getTime();
             //Make sure the save frequency has elapsed since the last update
             if(thisTime > (lastChecked+saveFrequency)){
@@ -36,72 +139,6 @@
         });
 
         $(options.selector).trigger('change keyup');
-
-
-        // Creates TinyMCE defaults, allowing user override, except for selector. activeType defaults to plain textarea with html capabilities
-        
-
-        //setInterval(updateEditor,0);   
-
-        //The order of the regex is important. For example, if I process paragraph first, the <p> tags will exists in
-        // elements that shouldn't contain them.
-        var defaultRegex = {
-                /* Header */    '(#+)(.*)':                         header,
-                /* Bold */      '(\\*\\*|__)(.*?)\\1':              '<strong>$2</strong>',
-                /* Link */      '\\[([^\\[]+)\\]\\(([^\\)]+)\\)':   '<a href=\'$2\'>$1</a>',
-                /* Hor. Rule */ '\\n-{5,}':                         '\n<hr />',
-                /* Code */      '`(.*?)`':                          '<code>$1</code>',
-                /* List UL */   '((?:\\*.*\\n)+)':                  list_ul,
-                /* List OL */   '((?:[0-9]+\\..*\\n)+)':            list_ol,
-                /* Blockquote */'\n(&gt;|\>)(.*)':                  '\n<blockquote>$2</blockquote>',
-                /* Paragraph */ '\\n([^\\n]+)\\n':                  paragraph,
-    };
-        function header(match, contents, offset, input_string){
-            var num = contents.length;
-            return '<h' + num + '>'+offset.trim()+'</h' + num + '>';
-        }
-        function list_ul(entire_group){
-            // Returns the entire li group since the regex grabbed all the items in the grouping
-            // Now I can add <ul> around the results and <li> tags around each list item
-
-            var result = entire_group.trim();
-            result = result.replace(/\*(.*)/g,'<li>$1</li>');
-            return '<ul>'+result+'</ul>';
-        }
-
-        function list_ol(entire_group){
-            // Returns the entire li group since the regex grabbed all the items in the grouping
-            // Now I can add <ol> around the results and <li> tags around each list item
-
-            var result = entire_group.trim();
-            result = result.replace(/^[0-9]*\.(.*)/gm,'<li>$1</li>');
-            return '<ol>'+result+'</ol>';
-        }
-
-        function paragraph(match, content) {
-            var result = content.trim();
-            // Prevent insertion of <p> tag inside an existing tag that should not contain p tags
-            if (/^<\/?(h|p|bl|ul|ol|li)/i.test(result)) {
-                return '\n' + content + '\n';
-            }
-            return '\n<p>' + result + '</p>\n';
-        }
-
-        function updatePreview(){
-            var originalText = $(options.textarea_selector).val();
-            var resultText = originalText;
-            //var arrayOfLines = lineString.match(/[^\r\n]+/g);
-            //Process each regular expressions
-            $.each(defaultRegex,function(expression,replacement){
-                var regex = new RegExp(expression,"gm");
-                //Process the regex against the input data.
-                resultText = resultText.replace(regex,replacement);
-            });
-
-
-            //Put the results back onto the pages preview container.
-            $(options.preview_selector).html(resultText);
-        }
 
     }
 
